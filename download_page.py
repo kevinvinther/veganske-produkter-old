4cb247bc43df077957d255d23eca57fca2c6f048
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2023-05-26 19:56:20 +0200

    Add all Naturli' products

--- download_page.py
+++ download_page.py
@@ -5,13 +5,14 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
 # Base URL of the website
-base_url = "https://www.schulstad.dk/vores-brod/alle-brod/rugbrod/vegansk/"
+base_url = "https://www.naturli-foods.dk/produkter/"
 # Directory to save images
 image_dir = './public/assets'
 
 # Ensure directory exists
 os.makedirs(image_dir, exist_ok=True)
 
+print("Fetching webpage...")
 response = requests.get(base_url)
 response.encoding = 'utf-8'
 
@@ -24,30 +25,36 @@ soup = BeautifulSoup(html, 'html.parser')
 products = []
 
 # Find all product cards
-for card in soup.find_all(class_='card card-product'):
+for product in soup.find_all(class_='product small'):
     # Get title
-    title = card.find(class_='card-product-title').text
+    title = " ".join(product.find(class_='product-title').text.split())
+    print(f"Found product: {title}")
     # Get image src
-    img_src = card.find('img')['src'] if card.find('img').get('src') else card.find('img')['srcset'].split(',')[0].split(' ')[0]
+    img_src = product.find('img').get('data-src')
+    print(f"Found image source: {img_src}")
     # Join base_url with image src to get full URL
     img_url = urljoin(base_url, img_src)
+    print(f"Constructed image URL: {img_url}")
     # Download image
+    print(f"Downloading image...")
     img_data = requests.get(img_url).content
     # Generate image name
     base_name, ext = os.path.splitext(img_src.split('/')[-1].split('?')[0])
     img_name = os.path.join(image_dir, base_name + ext)
+    print(f"Saving image as: {img_name}")
     # Write image to file
     with open(img_name, 'wb') as f:
         f.write(img_data)
+    print(f"Image saved.")
     # Create JSON object
     product = {
         'title': title,
         'image': img_name,
         'description': title + " er vegansk.",
-        'proof_text': "https://www.schulstad.dk/vores-brod/alle-brod/rugbrod/vegansk/",
+        'proof_text': "Alt fra naturlig er vegansk.",
         'proof_image': '',
         "category": [
-                "Brød"
+                ""
         ],
         "locations": [
             "",
@@ -57,4 +64,5 @@ for card in soup.find_all(class_='card card-product'):
     products.append(product)
 
 # Print JSON objects
+print("Final product list:")
 print(json.dumps(products, ensure_ascii=False, indent=4))
